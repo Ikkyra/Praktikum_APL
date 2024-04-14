96aa8cfa34b7b5da0c72bca19c54de4653d26219
Rifki Abiyan <rifki.abiyann@gmail.com>
2024-04-14 16:40:13 +0800

    added some frame to make early structure to the code.

--- Personal/pomotest.cpp
+++ Personal/pomotest.cpp
@@ -1,14 +1,36 @@
+// pomodoro
+//// start 
+//// pause
+//// resume
+// Break
+//// start
+//// pause
+//// resume
+// Long Break
+//// start
+//// pause
+//// resume
+// settings
+//// set work time
+//// set break time
+//// set long break time
+
 #include <iostream>
 #include <thread>
 #include <chrono>
 using namespace std;
+using namespace this_thread;
+
+void pomodoro() {
+    int workTime = 25;
+}
 
 int main() {
     int time = 60;
-    std::this_thread::sleep_for(1s);
+    sleep_for(1s);
     while(time >= 0) {
         cout << "\rSisa Waktu: " << time << flush;
-        std::this_thread::sleep_for(1s);    
+        sleep_for(1s);    
         time--;
     } 
 }
